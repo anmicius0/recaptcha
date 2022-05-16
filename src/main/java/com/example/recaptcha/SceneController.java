61d32e434cce9260d4e5574ff444b42f1c9fb871
anmicius0 <anmicius@disroot.org>
2022-05-16 11:04:43 +0800

    Simplify SceneController

--- src/main/java/com/example/recaptcha/SceneController.java
+++ src/main/java/com/example/recaptcha/SceneController.java
@@ -8,15 +8,12 @@ import java.io.IOException;
 
 public class SceneController {
     @FXML
-    private static GameScene gameScene;
-
-    @FXML
     private Label welcomeText;
 
     @FXML
     protected void onWelcomeButtonClick() throws IOException {
         // Change scene from welcome to game
         Stage stage = (Stage) welcomeText.getScene().getWindow();
-        gameScene = new GameScene(stage);
+        new GameScene(stage);
     }
 }
