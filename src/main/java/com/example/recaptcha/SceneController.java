e565a53dbddae5204500b9dd756ecae748bcaf51
anmicius0 <anmicius@disroot.org>
2022-05-16 10:08:39 +0800

    Combine SceneSwitcher.java and welcomeController.java into SceneController

---
+++ src/main/java/com/example/recaptcha/SceneController.java
@@ -0,0 +1,22 @@
+package com.example.recaptcha;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneController {
+    @FXML
+    private static GameScene gameScene;
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onWelcomeButtonClick() throws IOException {
+        Stage stage = (Stage) welcomeText.getScene().getWindow();
+        gameScene = new GameScene();
+        gameScene.init(stage);
+    }
+}
