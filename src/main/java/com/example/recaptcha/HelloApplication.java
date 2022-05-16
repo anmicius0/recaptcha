831c5390f3b4b6b39a5da49bb9f2a27ef26b9989
anmicius0 <anmicius@disroot.org>
2022-05-16 09:31:01 +0800

    Set welcome screen size to Maximized

--- src/main/java/com/example/recaptcha/HelloApplication.java
+++ src/main/java/com/example/recaptcha/HelloApplication.java
@@ -8,16 +8,15 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1100, 1160);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("welcome.fxml"));
+        stage.setMaximized(true);
+        stage.setScene(new Scene(fxmlLoader.load()));
+        stage.show();
     }
 }
