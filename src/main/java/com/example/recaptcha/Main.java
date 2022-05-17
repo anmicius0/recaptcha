f2865ae2ae53686cff39e5da35277790d96bb10f
anmicius0 <anmicius@disroot.org>
2022-05-17 14:13:38 +0800

    Implements SceneSwitcher in Main.java

--- src/main/java/com/example/recaptcha/Main.java
+++ src/main/java/com/example/recaptcha/Main.java
@@ -1,11 +1,7 @@
 package com.example.recaptcha;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -13,10 +9,7 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("welcome.fxml"));
-        stage.setMaximized(true);
-        stage.setScene(new Scene(fxmlLoader.load()));
-        stage.show();
+    public void start(Stage stage) {
+        new SceneSwitcher(stage);
     }
 }
