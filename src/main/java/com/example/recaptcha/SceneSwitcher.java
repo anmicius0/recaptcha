0030ac1c37c347ead1d9feb5563420ade3dff4be
anmicius0 <anmicius@disroot.org>
2022-05-17 14:13:54 +0800

    Initial version of SceneSwitcher.java

---
+++ src/main/java/com/example/recaptcha/SceneSwitcher.java
@@ -0,0 +1,44 @@
+package com.example.recaptcha;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneSwitcher {
+    private static Stage stage;
+    private static FXMLLoader loader;
+    private static Scene scene;
+
+    // Initialize the stage and show welcome screen
+    public SceneSwitcher(Stage stage) {
+        // Set stage
+        SceneSwitcher.stage = stage;
+        stage.setMaximized(true);
+
+        // Load welcome scene
+        loader = new FXMLLoader(SceneSwitcher.class.getResource("welcome.fxml"));
+        try {
+            scene = new Scene(loader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void switchScene(String sceneName) throws IOException {
+        // Decide scene
+        if (sceneName.equals("game")) {
+            loader = new FXMLLoader(Main.class.getResource("game.fxml"));
+            scene = new Scene(loader.load(), stage.getWidth(), stage.getHeight());
+        }
+        if (sceneName.equals("end")) {
+            loader = new FXMLLoader(Main.class.getResource("endgame.fxml"));
+            scene = new Scene(loader.load(), stage.getWidth(), stage.getHeight());
+        }
+
+        stage.setScene(scene);
+    }
+}
