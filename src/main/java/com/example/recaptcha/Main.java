e1e519a965c87235eef5dcb22f12e520e8db7d63
Anmicius <anmicius@disroot.org>
2022-05-18 09:34:25 +0800

    Change main application class from HelloApplication to Main

--- src/main/java/com/example/recaptcha/Main.java
+++ src/main/java/com/example/recaptcha/Main.java
@@ -7,14 +7,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     public static void main(String[] args) {
         launch();
     }
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("welcome.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("welcome.fxml"));
         stage.setMaximized(true);
         stage.setScene(new Scene(fxmlLoader.load()));
         stage.show();
