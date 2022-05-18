24a7e0666c1263396fbb81fc8b6b09bc8cd3b4f0
wenhsing <wenhsing@192.168.100.3>
2022-05-18 16:50:09 +0800

    try

--- src/main/java/com/example/recaptcha/Main.java
+++ src/main/java/com/example/recaptcha/Main.java
@@ -2,6 +2,7 @@ package com.example.recaptcha;
 
 import javafx.application.Application;
 import javafx.scene.image.Image;
+//import javafx.scene.media.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,6 +17,13 @@ public class Main extends Application {
 		//Image icon = new Image("icon.png");
 		//stage.getIcons().add(icon);
 		stage.setTitle("Recaptcha game");
+		
+		//this is bgm
+		//String music = this.getClass().getClassLoader().getResource("backgroundmusic.mp3").toExternalForm();
+		//Media media = new Media(music);
+		//MediaPlayer mediaplayer = new MediaPlayer(media);
+		//mediaplayer.setCycleCount(MediaPlayer.INDEFINITE);
+		//mediaplayer.play();
 		
         SceneSwitcher.setStage(stage);
     }
