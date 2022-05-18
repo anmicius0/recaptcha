0772bf6879bce3649d069a4fffd3de78374d6bba
Anmicius <anmicius@disroot.org>
2022-05-18 22:53:38 +0800

    Rule.java: Optimize import

--- src/main/java/com/example/recaptcha/Rule.java
+++ src/main/java/com/example/recaptcha/Rule.java
@@ -1,22 +1,18 @@
 package com.example.recaptcha;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class Rule {
 
-	  @FXML
-	    protected void RuleButtonClink() {
-	    	 // Change scene from rule to welcome
-	        try {
-	            SceneSwitcher.switchScene("welcome");
-	        } catch (IOException e) {
-	            throw new RuntimeException(e);
-	        }
-	    }
+    @FXML
+    protected void RuleButtonClink() {
+        // Change scene from rule to welcome
+        try {
+            SceneSwitcher.switchScene("welcome");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
