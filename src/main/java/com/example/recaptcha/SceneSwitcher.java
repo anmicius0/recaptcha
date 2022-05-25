aae60e6110b4e19efed701ef6c3985e6d6929d0d
Anmicius <anmicius@disroot.org>
2022-05-25 19:41:09 +0800

    SceneSwitcher.java won't throw IOException now

--- src/main/java/com/example/recaptcha/SceneSwitcher.java
+++ src/main/java/com/example/recaptcha/SceneSwitcher.java
@@ -2,41 +2,44 @@ package com.example.recaptcha;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class SceneSwitcher {
     private static Stage stage;
     private static FXMLLoader loader;
     private static Scene scene;
 
-    public static void switchScene(String sceneName) throws IOException {
-        // Decide scene
-        if (sceneName.equals("game")) {
-            loader = new FXMLLoader(Main.class.getResource("game.fxml"));
-            scene = new Scene(loader.load(), stage.getWidth(), stage.getHeight());
-        }
-        if (sceneName.equals("end")) {
-            loader = new FXMLLoader(Main.class.getResource("endgame.fxml"));
-            scene = new Scene(loader.load(), stage.getWidth(), stage.getHeight());
-            scene.getStylesheets().add(Main.class.getResource("endgame.css").toExternalForm());
-        }
-        if (sceneName.equals("rule")) {
-            loader = new FXMLLoader(Main.class.getResource("Rule.fxml"));
-            scene = new Scene(loader.load(), stage.getWidth(), stage.getHeight());
-            scene.getStylesheets().add(Main.class.getResource("rule.css").toExternalForm());
-        }
-        if (sceneName.equals("welcome")) {
-            loader = new FXMLLoader(Main.class.getResource("welcome.fxml"));
-            scene = new Scene(loader.load(), stage.getWidth(), stage.getHeight());
-            scene.getStylesheets().add(Main.class.getResource("welcome.css").toExternalForm());
+    public static void switchScene(String sceneName) {
+        try {
+            // Decide scene
+            if (sceneName.equals("game")) {
+                loader = new FXMLLoader(Main.class.getResource("game.fxml"));
+                scene = new Scene(loader.load(), stage.getWidth(), stage.getHeight());
+            }
+            if (sceneName.equals("end")) {
+                loader = new FXMLLoader(Main.class.getResource("endgame.fxml"));
+                scene = new Scene(loader.load(), stage.getWidth(), stage.getHeight());
+                scene.getStylesheets().add(Objects.requireNonNull(Main.class.getResource("endgame.css")).toExternalForm());
+            }
+            if (sceneName.equals("rule")) {
+                loader = new FXMLLoader(Main.class.getResource("Rule.fxml"));
+                scene = new Scene(loader.load(), stage.getWidth(), stage.getHeight());
+                scene.getStylesheets().add(Objects.requireNonNull(Main.class.getResource("rule.css")).toExternalForm());
+            }
+            if (sceneName.equals("welcome")) {
+                loader = new FXMLLoader(Main.class.getResource("welcome.fxml"));
+                scene = new Scene(loader.load(), stage.getWidth(), stage.getHeight());
+                scene.getStylesheets().add(Objects.requireNonNull(Main.class.getResource("welcome.css")).toExternalForm());
 
-        }
+            }
 
-        stage.setScene(scene);
+            stage.setScene(scene);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // Initialize the stage and show welcome screen
@@ -49,7 +52,7 @@ public class SceneSwitcher {
         loader = new FXMLLoader(SceneSwitcher.class.getResource("welcome.fxml"));
         try {
             scene = new Scene(loader.load());
-            scene.getStylesheets().add(Main.class.getResource("welcome.css").toExternalForm());
+            scene.getStylesheets().add(Objects.requireNonNull(Main.class.getResource("welcome.css")).toExternalForm());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
